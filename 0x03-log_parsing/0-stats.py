9a0589fea521b79f114e2534ec9613bce1acb117
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-20 19:15:07 +0100

    Task 00

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,52 @@
 #!/usr/bin/python3
-"""0. Log parsing"""
+"""Parse a log file"""
+
 import sys
 
-status_codes = {}
-total_size = 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+
+line_count = 0
+total_file_size = 0
 
 
-def add(status: str, size: str) -> int:
-    """Updates log metrics"""
-    try:
-        status = int(status)
-        size = int(size)
-    except:
-        return 0
-    arr = [200, 301, 400, 401, 403, 404, 405, 500]
-    if status not in arr:
-        return
-    if not status_codes.get(status, None):
-        status_codes.update({status: 1})
-    else:
-        status_codes.update({status: status_codes.get(status) + 1})
-    return size
+def print_stats():
+    print("File size: {:d}".format(total_file_size))
+    for key, value in status_codes.items():
+        if value:
+            print("{}: {:d}".format(key, value))
 
-
-def print_log() -> None:
-    """Prints Log metrics"""
-    print('File size: {}'.format(total_size))
-    arr = list(status_codes.items())
-    arr.sort()
-    for status, counts in arr:
-        print('{}: {}'.format(status, counts))
-
-
-DEFAULT_COUNT = 10
-log_interval = DEFAULT_COUNT
 
 try:
     for line in sys.stdin:
-        total_size += add(*(line.split()[-2:]))
-        if log_interval == 1:
-            log_interval = DEFAULT_COUNT
-            print_log()
-        else:
-            log_interval -= 1
+        line_count += 1
+        line = line[:-1]
+        array = line.split()
+        file_size = 0
+        try:
+            file_size = int(array[-1])
+        except:
+            pass
+        # calc stats
+        total_file_size += file_size
+        try:
+            status_code = str(array[-2])
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except:
+            pass
+
+        if (line_count % 10 == 0):
+            print_stats()
+    print_stats()
 except KeyboardInterrupt:
-    print_log()
+    print_stats()
     raise
