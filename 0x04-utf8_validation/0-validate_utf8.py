4b61a835329d8dd3e44a2926907a168c3e9afd73
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-27 06:39:08 +0100

    Task 00

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,16 +7,21 @@ def validUTF8(data):
     valid UTF-8 encoding"""
 
     def check_next_bytes(num_bytes):
+        """Inspects following data continuation"""
         nonlocal i
         for _ in range(num_bytes):
-            if i >= len(data) or (data[i] >> 6) != 0b10:
+            if i >= len(data) or (get8(data[i]) >> 6) != 0b10:
                 return False
             i += 1
         return True
 
+    def get8(byte):
+        """Returns the 8 least significant bits"""
+        return (int(format(byte, '08b')[-8:], 2))
+
     i = 0
     while i < len(data):
-        byte = data[i]
+        byte = get8(data[i])
         if (byte >> 7) == 0:  # 1-byte character (0xxxxxxx)
             i += 1
         elif (byte >> 5) == 0b110:  # 2-byte character (110xxxxx)
