f3cb85ba9d18a55387c60b1e4f5af896d6d55775
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-07-27 06:44:15 +0100

    Task 00

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,35 +6,20 @@ def validUTF8(data):
     """Determines if a given data set represents a
     valid UTF-8 encoding"""
 
-    def check_next_bytes(num_bytes):
-        """Inspects following data continuation"""
-        nonlocal i
-        for _ in range(num_bytes):
-            if i >= len(data) or (get8(data[i]) >> 6) != 0b10:
-                return False
-            i += 1
-        return True
+    byte_count = 0
 
-    def get8(byte):
-        """Returns the 8 least significant bits"""
-        return (int(format(byte, '08b')[-8:], 2))
-
-    i = 0
-    while i < len(data):
-        byte = get8(data[i])
-        if (byte >> 7) == 0:  # 1-byte character (0xxxxxxx)
-            i += 1
-        elif (byte >> 5) == 0b110:  # 2-byte character (110xxxxx)
-            if not check_next_bytes(1):
-                return False
-        elif (byte >> 4) == 0b1110:  # 3-byte character (1110xxxx)
-            if not check_next_bytes(2):
-                return False
-        elif (byte >> 3) == 0b11110:  # 4-byte character (11110xxx)
-            if not check_next_bytes(3):
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            return False
-        i += 1
-
-    return True
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0
