bf57929b6db6fb67f72f865b8946827c0cdadb65
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-24 22:12:02 +0100

    Task 00

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,7 @@
 def makeChange(coins, total):
     """determines the fewest number of coins needed to meet a
     given amount total"""
-    
+
     if total <= 0:
         return 0
     coins.sort(reverse=True)
