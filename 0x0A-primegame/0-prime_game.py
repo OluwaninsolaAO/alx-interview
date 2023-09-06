5ce554bdb8eecb2bc8e890171a9ccbcb7ee973ec
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-09-06 20:57:08 +0100

    Task 00

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""0. The Prime Game"""
+
+
+def isWinner(x: int, nums: list[int]) -> str:
+    """determine who the winner of each game is between Ben and Maria"""
+    ben = 0
+    maria = 0
+    index = 1
+
+    for round in range(x):
+        arr = list(range(2, nums[round] + 1))
+
+        while len(arr) != 0:
+            prime = arr[0]
+            for num in arr[:]:
+                if num % prime == 0:
+                    arr.pop(arr.index(num))
+            if index % 2 == 1:
+                maria += 1
+            else:
+                ben += 1
+
+    # return results
+    if ben > maria:
+        return 'Ben'
+    elif maria > ben:
+        return 'Maria'
+    elif ben == 0 and maria == 0:
+        return 'Ben'
+    return None
