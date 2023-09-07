d4564017a13b5f319f075a2f4744dc6b6c7dcf62
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-09-07 11:57:28 +0100

    Task 00

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,24 +4,30 @@
 
 def isWinner(x: int, nums: list) -> str:
     """determine who the winner of each game is between Ben and Maria"""
+    #  print('x:', x, 'nums:', nums)
     ben = 0
     maria = 0
-    index = 1
 
     for round in range(x):
         arr = list(range(2, nums[round] + 1))
+        #  print('Round', round + 1, ':', arr)
 
+        index = 0
         while len(arr) != 0:
+            index += 1
+            #  print('Maria\'s' if index % 2 == 1 else 'Ben\'s', 'move:', arr)
             prime = arr[0]
             for num in arr[:]:
                 if num % prime == 0:
                     arr.pop(arr.index(num))
-            index += 1
         if index % 2 == 1:
             maria += 1
+            #  print('Maria won!', '>>>', 'Ben:', ben, 'Maria:', maria)
         else:
             ben += 1
+            #  print('Ben won!', '>>>', 'Ben:', ben, 'Maria:', maria)
 
+    #  print('Ben:', ben, 'Maria:', maria)
     # return results
     if ben > maria:
         return 'Ben'
