08d9f1bd7b05392b30a64e5b5246a5fd7322d7a9
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-09-07 12:05:10 +0100

    Task 00

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,30 +4,30 @@
 
 def isWinner(x: int, nums: list) -> str:
     """determine who the winner of each game is between Ben and Maria"""
-    #  print('x:', x, 'nums:', nums)
+    print('x:', x, 'nums:', nums)
     ben = 0
     maria = 0
 
     for round in range(x):
-        arr = list(range(2, nums[round] + 1))
-        #  print('Round', round + 1, ':', arr)
+        arr = list(range(1, nums[round] + 1))
+        print('Round', round + 1, ':', arr)
 
         index = 0
-        while len(arr) != 0:
+        while len(arr) != 1:
             index += 1
-            #  print('Maria\'s' if index % 2 == 1 else 'Ben\'s', 'move:', arr)
-            prime = arr[0]
+            print('Maria\'s' if index % 2 == 1 else 'Ben\'s', 'move:', arr)
+            prime = arr[1]
             for num in arr[:]:
                 if num % prime == 0:
                     arr.pop(arr.index(num))
         if index % 2 == 1:
             maria += 1
-            #  print('Maria won!', '>>>', 'Ben:', ben, 'Maria:', maria)
+            print('Maria won!', '>>>', 'Ben:', ben, 'Maria:', maria)
         else:
             ben += 1
-            #  print('Ben won!', '>>>', 'Ben:', ben, 'Maria:', maria)
+            print('Ben won!', '>>>', 'Ben:', ben, 'Maria:', maria)
 
-    #  print('Ben:', ben, 'Maria:', maria)
+    print('Ben:', ben, 'Maria:', maria)
     # return results
     if ben > maria:
         return 'Ben'
