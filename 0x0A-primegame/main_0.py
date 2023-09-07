840a6eff1e9d4d3e27745db354ecac067bbe50fe
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-09-07 11:58:48 +0100

    Task 00

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 isWinner = __import__('0-prime_game').isWinner
-print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))  # Maria
+print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))  # Ben
 print("Winner: {}".format(isWinner(3, [4, 5, 1])))  # Ben
 print("Winner: {}".format(isWinner(2, [1, 2])))  # None
 print("Winner: {}".format(isWinner(2, [2, 2])))  # Maria
